89c0b9bf4dacce0bd8f97ab2ec2569e44d96457f
alexwilhelm663-svg <alexwilhelm663@gmail.com>
2026-07-05 20:30:57 +0200

    Update engine.ts

--- src/engine.ts
+++ src/engine.ts
@@ -1,5 +1,5 @@
 // @ts-nocheck
-import { ewAnalyzerWorkflow } from "./ewValidator (1)";
+import { ewAnalyzerWorkflow } from "./ewValidator";
 import db from "./db"; // Import aus deinem neuen Lern-Modul
 import fs from "fs";
 import path from "path";
